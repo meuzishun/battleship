08c8b976926f3dd0a06537d088f07536068c32aa
Andrew Smith <meuzishun@gmail.com>
2022-01-26 15:27:04 -0500

    add gameboard files

---
+++ gameboard.test.js
@@ -0,0 +1,4 @@
+// TODO: should be able to place ships at specific coordinates by calling the ship factory
+// TODO: should have a receiveAttack function that takes a pair of coordinates, determines whether or not the attack hit a ship and then sends the ‘hit’ function to the correct ship, or records the coordinates of the missed shot
+// TODO: should keep track of missed attacks so they can display them properly
+// TODO: should be able to report whether or not all of their ships have been sunk
