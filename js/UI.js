a7a3154097f090e423f2ed3a72cb36d392aebb6b
Andrew Smith <meuzishun@gmail.com>
2022-02-28 17:12:47 -0500

    convert to IIFE without any other changes

--- js/UI.js
+++ js/UI.js
@@ -1,112 +1,114 @@
 import { gameLoop } from './gameLoop.js';
 
 //TODO: IIFEs can prevent leaking methods...
-const UI = {
-  gameContainer: document.querySelector('.game-container'),
-  gameboardSides: [],
+const UI = (function () {
+  return {
+    gameContainer: document.querySelector('.game-container'),
+    gameboardSides: [],
 
-  createHeader: function () {
-    const gameboardHeader = document.createElement('header');
-    gameboardHeader.classList.add('gameboard-header');
-    gameboardHeader.textContent = 'Battleship';
-    this.gameContainer.appendChild(gameboardHeader);
-  },
+    createHeader: function () {
+      const gameboardHeader = document.createElement('header');
+      gameboardHeader.classList.add('gameboard-header');
+      gameboardHeader.textContent = 'Battleship';
+      this.gameContainer.appendChild(gameboardHeader);
+    },
 
-  createMessageWindow: function () {
-    const messageContainer = document.createElement('div');
-    messageContainer.classList.add('message-container');
-    const messageText = document.createElement('p');
-    messageText.classList.add('message-text');
-    messageContainer.appendChild(messageText);
-    this.gameContainer.appendChild(messageContainer);
-    this.messageText = messageText;
-  },
+    createMessageWindow: function () {
+      const messageContainer = document.createElement('div');
+      messageContainer.classList.add('message-container');
+      const messageText = document.createElement('p');
+      messageText.classList.add('message-text');
+      messageContainer.appendChild(messageText);
+      this.gameContainer.appendChild(messageContainer);
+      this.messageText = messageText;
+    },
 
-  clearMessage: function () {
-    this.messageText.textContent = '';
-  },
+    clearMessage: function () {
+      this.messageText.textContent = '';
+    },
 
-  displayMessage: function (msg) {
-    this.messageText.textContent = msg;
-    const msgTimer = setTimeout(function () {
-      UI.clearMessage();
-      clearTimeout(msgTimer);
-    }, 3000);
-  },
+    displayMessage: function (msg) {
+      this.messageText.textContent = msg;
+      const msgTimer = setTimeout(function () {
+        UI.clearMessage();
+        clearTimeout(msgTimer);
+      }, 3000);
+    },
 
-  updateCellStatus: function (cell, status) {
-    cell.classList.add(status);
-  },
+    updateCellStatus: function (cell, status) {
+      cell.classList.add(status);
+    },
 
-  createBoard: function () {
-    const gameboard = document.createElement('div');
-    gameboard.classList.add('gameboard');
+    createBoard: function () {
+      const gameboard = document.createElement('div');
+      gameboard.classList.add('gameboard');
 
-    for (let i = 0; i < 100; i++) {
-      const cell = document.createElement('div');
-      cell.classList.add('cell');
-      cell.dataset.position = i;
-      gameboard.appendChild(cell);
-    }
+      for (let i = 0; i < 100; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.dataset.position = i;
+        gameboard.appendChild(cell);
+      }
 
-    return gameboard;
-  },
+      return gameboard;
+    },
 
-  handleBoardClick: function (e) {
-    if (!e.target.classList.contains('cell')) {
-      return;
-    }
+    handleBoardClick: function (e) {
+      if (!e.target.classList.contains('cell')) {
+        return;
+      }
 
-    const cell = e.target;
+      const cell = e.target;
 
-    if (cell.classList.contains('hit')) {
-      return;
-    }
+      if (cell.classList.contains('hit')) {
+        return;
+      }
 
-    if (cell.classList.contains('miss')) {
-      return;
-    }
+      if (cell.classList.contains('miss')) {
+        return;
+      }
 
-    gameLoop.processTurn(cell);
-  },
+      gameLoop.processTurn(cell);
+    },
 
-  createShipList: function () {
-    const listContainer = document.createElement('div');
-    listContainer.classList.add('ship-list');
-    return listContainer;
-  },
+    createShipList: function () {
+      const listContainer = document.createElement('div');
+      listContainer.classList.add('ship-list');
+      return listContainer;
+    },
 
-  addShipToList: function (ship, list) {
-    const shipName = document.createElement('p');
-    shipName.classList.add('ship-name');
-    shipName.dataset.name = ship.name;
-    shipName.textContent = ship.name;
-    list.appendChild(shipName);
-  },
+    addShipToList: function (ship, list) {
+      const shipName = document.createElement('p');
+      shipName.classList.add('ship-name');
+      shipName.dataset.name = ship.name;
+      shipName.textContent = ship.name;
+      list.appendChild(shipName);
+    },
 
-  markShipAsSunk: function (ship, list) {},
+    markShipAsSunk: function (ship, list) {},
 
-  createBoardSide: function (side) {
-    const boardSide = document.createElement('div');
-    boardSide.classList.add('gameboard-container');
-    boardSide.classList.add(`${side}-gameboard-container`);
+    createBoardSide: function (side) {
+      const boardSide = document.createElement('div');
+      boardSide.classList.add('gameboard-container');
+      boardSide.classList.add(`${side}-gameboard-container`);
 
-    const shipList = this.createShipList();
-    const gameboard = this.createBoard();
-    boardSide.appendChild(shipList);
-    //? We are going to query the DOM to get to the list and board but would it be better to add a property?  Probably not...
-    boardSide.appendChild(gameboard);
-    this.gameContainer.appendChild(boardSide);
-    this.gameboardSides.push({ boardSide, gameboard, shipList });
-  },
+      const shipList = this.createShipList();
+      const gameboard = this.createBoard();
+      boardSide.appendChild(shipList);
+      //? We are going to query the DOM to get to the list and board but would it be better to add a property?  Probably not...
+      boardSide.appendChild(gameboard);
+      this.gameContainer.appendChild(boardSide);
+      this.gameboardSides.push({ boardSide, gameboard, shipList });
+    },
 
-  init: function () {
-    this.createHeader();
-    this.createMessageWindow();
-    this.createBoardSide('left-side');
-    this.createBoardSide('right-side');
-  },
-};
+    init: function () {
+      this.createHeader();
+      this.createMessageWindow();
+      this.createBoardSide('left-side');
+      this.createBoardSide('right-side');
+    },
+  };
+})();
 
 UI.init();
 
--- js/gameLoop.js
+++ js/gameLoop.js
@@ -2,105 +2,107 @@ import { createPlayer } from './factories/playerFactory.js';
 import { UI } from './UI.js';
 
 //TODO: IIFEs can prevent leaking methods...
-const gameLoop = {
-  players: [],
-  currentPlayer: undefined,
-  opponent: undefined,
+const gameLoop = (function () {
+  return {
+    players: [],
+    currentPlayer: undefined,
+    opponent: undefined,
 
-  addPlayerToGame: function (player, UI_board_side) {
-    this.players.push({ player, UI_board_side });
-  },
+    addPlayerToGame: function (player, UI_board_side) {
+      this.players.push({ player, UI_board_side });
+    },
 
-  setFirstTurn: function () {
-    [this.currentPlayer, this.opponent] = this.players;
-    this.opponent.UI_board_side.gameboard.addEventListener(
-      'click',
-      UI.handleBoardClick
-    );
-  },
-
-  handleAddPlayerSubmission: function (e) {
-    e.preventDefault();
-    // get player info
-  },
-
-  switchTurn: function () {
-    this.opponent.UI_board_side.gameboard.removeEventListener(
-      'click',
-      UI.handleBoardClick
-    );
-
-    [this.opponent, this.currentPlayer] = [this.currentPlayer, this.opponent];
-
-    this.opponent.UI_board_side.gameboard.addEventListener(
-      'click',
-      UI.handleBoardClick
-    );
-  },
-
-  //TODO: this seems like a UI method...
-  markShipSunk: function (board, ship) {
-    ship.positions.forEach((position) => {
-      const cell = board.querySelector(`[data-position='${position}']`);
-      cell.classList.remove('hit');
-      cell.classList.add('sunk');
-    });
-    board.parentElement
-      .querySelector('.ship-list')
-      .querySelector(`[data-name='${ship.name}']`)
-      .classList.add('crossout');
-  },
-
-  processTurn: function (cell) {
-    const position = Number(cell.dataset.position);
-    this.currentPlayer.player.attack(this.opponent.player, position);
-    const { occupied, status } = this.opponent.player.board.cells[position];
-
-    if (occupied && occupied.isSunk()) {
-      //TODO: this seems like a UI method...
-      this.markShipSunk(cell.parentElement, occupied);
-      UI.displayMessage(
-        `${this.currentPlayer.player.name} has sunk ${this.opponent.player.name}'s ${occupied.name}!`
+    setFirstTurn: function () {
+      [this.currentPlayer, this.opponent] = this.players;
+      this.opponent.UI_board_side.gameboard.addEventListener(
+        'click',
+        UI.handleBoardClick
       );
-    } else if (status === 'hit') {
-      cell.classList.add('hit');
-      UI.displayMessage('Hit!');
-    } else if (status === 'miss') {
-      cell.classList.add('miss');
-      UI.displayMessage('Miss...');
-    }
+    },
 
-    //TODO: check to see if all ships are sunk...
+    handleAddPlayerSubmission: function (e) {
+      e.preventDefault();
+      // get player info
+    },
 
-    this.switchTurn();
-  },
+    switchTurn: function () {
+      this.opponent.UI_board_side.gameboard.removeEventListener(
+        'click',
+        UI.handleBoardClick
+      );
 
-  temporaryPlaceShipsFunction: function () {
-    this.players[0].player.board.placeShip(21, 'Carrier', 'vertical');
-    this.players[1].player.board.placeShip(30, 'Carrier', 'vertical');
-    this.players[0].player.board.placeShip(53, 'Battleship', 'horizontal');
-    this.players[1].player.board.placeShip(23, 'Battleship', 'horizontal');
-    this.players[0].player.board.placeShip(3, 'Destroyer', 'horizontal');
-    this.players[1].player.board.placeShip(3, 'Destroyer', 'horizontal');
-    this.players[0].player.board.placeShip(75, 'Submarine', 'horizontal');
-    this.players[1].player.board.placeShip(84, 'Submarine', 'horizontal');
-    this.players[0].player.board.placeShip(39, 'Patrol Boat', 'vertical');
-    this.players[1].player.board.placeShip(78, 'Patrol Boat', 'vertical');
+      [this.opponent, this.currentPlayer] = [this.currentPlayer, this.opponent];
 
-    this.players.forEach((player, index) => {
-      player.player.board.ships.forEach((ship) => {
-        UI.addShipToList(ship, UI.gameboardSides[index].shipList);
+      this.opponent.UI_board_side.gameboard.addEventListener(
+        'click',
+        UI.handleBoardClick
+      );
+    },
+
+    //TODO: this seems like a UI method...
+    markShipSunk: function (board, ship) {
+      ship.positions.forEach((position) => {
+        const cell = board.querySelector(`[data-position='${position}']`);
+        cell.classList.remove('hit');
+        cell.classList.add('sunk');
       });
-    });
-  },
+      board.parentElement
+        .querySelector('.ship-list')
+        .querySelector(`[data-name='${ship.name}']`)
+        .classList.add('crossout');
+    },
 
-  init: function () {
-    //TODO: We should use a modal here to setup the players...
-    this.addPlayerToGame(createPlayer('Player 1'), UI.gameboardSides[0]);
-    this.addPlayerToGame(createPlayer('Player 2'), UI.gameboardSides[1]);
-    this.temporaryPlaceShipsFunction();
-    this.setFirstTurn();
-  },
-};
+    processTurn: function (cell) {
+      const position = Number(cell.dataset.position);
+      this.currentPlayer.player.attack(this.opponent.player, position);
+      const { occupied, status } = this.opponent.player.board.cells[position];
+
+      if (occupied && occupied.isSunk()) {
+        //TODO: this seems like a UI method...
+        this.markShipSunk(cell.parentElement, occupied);
+        UI.displayMessage(
+          `${this.currentPlayer.player.name} has sunk ${this.opponent.player.name}'s ${occupied.name}!`
+        );
+      } else if (status === 'hit') {
+        cell.classList.add('hit');
+        UI.displayMessage('Hit!');
+      } else if (status === 'miss') {
+        cell.classList.add('miss');
+        UI.displayMessage('Miss...');
+      }
+
+      //TODO: check to see if all ships are sunk...
+
+      this.switchTurn();
+    },
+
+    temporaryPlaceShipsFunction: function () {
+      this.players[0].player.board.placeShip(21, 'Carrier', 'vertical');
+      this.players[1].player.board.placeShip(30, 'Carrier', 'vertical');
+      this.players[0].player.board.placeShip(53, 'Battleship', 'horizontal');
+      this.players[1].player.board.placeShip(23, 'Battleship', 'horizontal');
+      this.players[0].player.board.placeShip(3, 'Destroyer', 'horizontal');
+      this.players[1].player.board.placeShip(3, 'Destroyer', 'horizontal');
+      this.players[0].player.board.placeShip(75, 'Submarine', 'horizontal');
+      this.players[1].player.board.placeShip(84, 'Submarine', 'horizontal');
+      this.players[0].player.board.placeShip(39, 'Patrol Boat', 'vertical');
+      this.players[1].player.board.placeShip(78, 'Patrol Boat', 'vertical');
+
+      this.players.forEach((player, index) => {
+        player.player.board.ships.forEach((ship) => {
+          UI.addShipToList(ship, UI.gameboardSides[index].shipList);
+        });
+      });
+    },
+
+    init: function () {
+      //TODO: We should use a modal here to setup the players...
+      this.addPlayerToGame(createPlayer('Player 1'), UI.gameboardSides[0]);
+      this.addPlayerToGame(createPlayer('Player 2'), UI.gameboardSides[1]);
+      this.temporaryPlaceShipsFunction();
+      this.setFirstTurn();
+    },
+  };
+})();
 
 export { gameLoop };
