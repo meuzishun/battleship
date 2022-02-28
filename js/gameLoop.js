cb15beb3bf2bc8b61a73f15eae86ae08122e82ae
Andrew Smith <meuzishun@gmail.com>
2022-02-28 17:47:13 -0500

    separate public and private gameloop methods

--- js/gameLoop.js
+++ js/gameLoop.js
@@ -3,40 +3,41 @@ import { UI } from './UI.js';
 
 //TODO: IIFEs can prevent leaking methods...
 const gameLoop = (function () {
+  const players = [];
+  let currentPlayer;
+  let opponent;
+
+  const setFirstTurn = function () {
+    [currentPlayer, opponent] = players;
+    opponent.UI_board_side.gameboard.addEventListener(
+      'click',
+      UI.handleBoardClick
+    );
+  };
+
+  const switchTurn = function () {
+    opponent.UI_board_side.gameboard.removeEventListener(
+      'click',
+      UI.handleBoardClick
+    );
+
+    [opponent, currentPlayer] = [currentPlayer, opponent];
+
+    opponent.UI_board_side.gameboard.addEventListener(
+      'click',
+      UI.handleBoardClick
+    );
+  };
+
   return {
-    players: [],
-    currentPlayer: undefined,
-    opponent: undefined,
-
+    //*
     addPlayerToGame: function (player, UI_board_side) {
-      this.players.push({ player, UI_board_side });
-    },
-
-    setFirstTurn: function () {
-      [this.currentPlayer, this.opponent] = this.players;
-      this.opponent.UI_board_side.gameboard.addEventListener(
-        'click',
-        UI.handleBoardClick
-      );
+      players.push({ player, UI_board_side });
     },
 
     handleAddPlayerSubmission: function (e) {
       e.preventDefault();
       // get player info
-    },
-
-    switchTurn: function () {
-      this.opponent.UI_board_side.gameboard.removeEventListener(
-        'click',
-        UI.handleBoardClick
-      );
-
-      [this.opponent, this.currentPlayer] = [this.currentPlayer, this.opponent];
-
-      this.opponent.UI_board_side.gameboard.addEventListener(
-        'click',
-        UI.handleBoardClick
-      );
     },
 
     //TODO: this seems like a UI method...
@@ -52,16 +53,17 @@ const gameLoop = (function () {
         .classList.add('crossout');
     },
 
+    //*
     processTurn: function (cell) {
       const position = Number(cell.dataset.position);
-      this.currentPlayer.player.attack(this.opponent.player, position);
-      const { occupied, status } = this.opponent.player.board.cells[position];
+      currentPlayer.player.attack(opponent.player, position);
+      const { occupied, status } = opponent.player.board.cells[position];
 
       if (occupied && occupied.isSunk()) {
         //TODO: this seems like a UI method...
         this.markShipSunk(cell.parentElement, occupied);
         UI.displayMessage(
-          `${this.currentPlayer.player.name} has sunk ${this.opponent.player.name}'s ${occupied.name}!`
+          `${currentPlayer.player.name} has sunk ${opponent.player.name}'s ${occupied.name}!`
         );
       } else if (status === 'hit') {
         cell.classList.add('hit');
@@ -73,34 +75,36 @@ const gameLoop = (function () {
 
       //TODO: check to see if all ships are sunk...
 
-      this.switchTurn();
+      switchTurn();
     },
 
+    //*
     temporaryPlaceShipsFunction: function () {
-      this.players[0].player.board.placeShip(21, 'Carrier', 'vertical');
-      this.players[1].player.board.placeShip(30, 'Carrier', 'vertical');
-      this.players[0].player.board.placeShip(53, 'Battleship', 'horizontal');
-      this.players[1].player.board.placeShip(23, 'Battleship', 'horizontal');
-      this.players[0].player.board.placeShip(3, 'Destroyer', 'horizontal');
-      this.players[1].player.board.placeShip(3, 'Destroyer', 'horizontal');
-      this.players[0].player.board.placeShip(75, 'Submarine', 'horizontal');
-      this.players[1].player.board.placeShip(84, 'Submarine', 'horizontal');
-      this.players[0].player.board.placeShip(39, 'Patrol Boat', 'vertical');
-      this.players[1].player.board.placeShip(78, 'Patrol Boat', 'vertical');
+      players[0].player.board.placeShip(21, 'Carrier', 'vertical');
+      players[1].player.board.placeShip(30, 'Carrier', 'vertical');
+      players[0].player.board.placeShip(53, 'Battleship', 'horizontal');
+      players[1].player.board.placeShip(23, 'Battleship', 'horizontal');
+      players[0].player.board.placeShip(3, 'Destroyer', 'horizontal');
+      players[1].player.board.placeShip(3, 'Destroyer', 'horizontal');
+      players[0].player.board.placeShip(75, 'Submarine', 'horizontal');
+      players[1].player.board.placeShip(84, 'Submarine', 'horizontal');
+      players[0].player.board.placeShip(39, 'Patrol Boat', 'vertical');
+      players[1].player.board.placeShip(78, 'Patrol Boat', 'vertical');
 
-      this.players.forEach((player, index) => {
+      players.forEach((player, index) => {
         player.player.board.ships.forEach((ship) => {
           UI.addShipToList(ship, UI.gameboardSides[index].shipList);
         });
       });
     },
 
+    //*
     init: function () {
       //TODO: We should use a modal here to setup the players...
       this.addPlayerToGame(createPlayer('Player 1'), UI.gameboardSides[0]);
       this.addPlayerToGame(createPlayer('Player 2'), UI.gameboardSides[1]);
       this.temporaryPlaceShipsFunction();
-      this.setFirstTurn();
+      setFirstTurn();
     },
   };
 })();
