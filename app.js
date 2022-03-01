a37d829c1d4bdc8ebe6f757183aad6d958182a9b
Andrew Smith <meuzishun@gmail.com>
2022-03-01 10:20:52 -0500

    move inits down so they are together

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 import { gameLoop } from './js/gameLoop.js';
 import { UI } from './js/UI.js';
 
-UI.init();
 gameLoop.addPlayerToGame('Player 1');
 gameLoop.addPlayerToGame('Player 2');
 
+UI.init();
 gameLoop.init();
