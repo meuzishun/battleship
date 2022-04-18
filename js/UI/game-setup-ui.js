5021587527f7c0c2ebfe8ed3c88731974ca4329c
Andrew Smith <meuzishun@gmail.com>
2022-04-18 15:39:05 -0400

    get drag and drop working

--- js/UI/game-setup-ui.js
+++ js/UI/game-setup-ui.js
@@ -2,12 +2,6 @@ import { boundEventListeners } from '../bound-event-listeners.js';
 import { gameLoop } from '../gameLoop.js';
 
 export const gameSetup_UI = (function () {
-  // let currentPlayer = undefined;
-  let messageText = undefined;
-  let msgTimer;
-
-  // const eventListenerCallbacks = {};
-
   const createSetupContainer = function () {
     const setupContainer = document.createElement('div');
     setupContainer.classList.add('setup-container');
@@ -17,31 +11,38 @@ export const gameSetup_UI = (function () {
   const createMessageWindow = function () {
     const messageContainer = document.createElement('div');
     messageContainer.classList.add('message-container');
-    messageText = document.createElement('p');
+    const messageText = document.createElement('p');
     messageText.classList.add('message-text');
     messageContainer.appendChild(messageText);
-    return messageContainer;
-  };
 
-  const clearMessage = function () {
-    messageText.textContent = '';
-  };
+    let msgTimer;
 
-  const startMsgTimer = function (time) {
-    msgTimer = setTimeout(clearMessage, time);
-  };
+    const clearMessage = function () {
+      messageText.textContent = '';
+    };
 
-  const cancelMsgTimer = function () {
-    clearTimeout(msgTimer);
-  };
+    const startMsgTimer = function (time) {
+      msgTimer = setTimeout(clearMessage, time);
+    };
 
-  const displayMessage = function (msg, delay) {
-    cancelMsgTimer();
-    clearMessage();
-    messageText.textContent = msg;
-    if (delay) {
-      startMsgTimer(delay);
-    }
+    const cancelMsgTimer = function () {
+      clearTimeout(msgTimer);
+    };
+
+    const displayMessage = function (msg, delay) {
+      cancelMsgTimer();
+      clearMessage();
+      messageText.textContent = msg;
+      if (delay) {
+        startMsgTimer(delay);
+        cancelMsgTimer();
+      }
+    };
+
+    return {
+      elem: messageContainer,
+      displayMessage,
+    };
   };
 
   const createShipPlacementList = function () {
@@ -55,16 +56,17 @@ export const gameSetup_UI = (function () {
 
     const listContainer = document.createElement('div');
     listContainer.classList.add('ship-placement-list');
+
     ships.forEach((ship) => {
       const shipText = document.createElement('p');
       shipText.classList.add('ship-name');
       shipText.dataset.name = ship.name;
       shipText.dataset.length = ship.length;
-      shipText.textContent = `${ship.name} (${ship.length})`;
+      shipText.textContent = `${ship.name}`;
       shipText.draggable = true;
       shipText.addEventListener(
         'dragstart',
-        (boundEventListeners[`${ship.name}Drag`] =
+        (boundEventListeners[`${ship.name} Drag Event`] =
           handleShipNameDrag.bind(this))
       );
       listContainer.appendChild(shipText);
@@ -88,35 +90,45 @@ export const gameSetup_UI = (function () {
     return gameboard;
   };
 
-  const initializeBoardForShipDrop = function (player) {
+  const buildSetupUI = function () {
     document.querySelector('.wrapper').textContent = '';
-    // currentPlayer = player;
-    // console.log(currentPlayer);
-    const setupContainer = createSetupContainer();
-    setupContainer.appendChild(createMessageWindow());
-    displayMessage(`Place the ships on the board for ${player.name}`);
 
-    const setupBoard = createBoard();
-    setupBoard.addEventListener(
+    const container = createSetupContainer();
+    const messageWindow = createMessageWindow();
+    const board = createBoard();
+    const shipList = createShipPlacementList();
+
+    container.appendChild(messageWindow.elem);
+    container.appendChild(shipList);
+    container.appendChild(board);
+
+    document.querySelector('.wrapper').appendChild(container);
+
+    return {
+      container,
+      messageWindow,
+      board,
+      shipList,
+    };
+  };
+
+  const initializeBoardForShipDrop = function (player) {
+    const setupUI = buildSetupUI();
+
+    setupUI.messageWindow.displayMessage(
+      `Place the ships on the board for ${player.name}`
+    );
+
+    setupUI.board.addEventListener(
       'dragenter',
       (boundEventListeners.setupBoardDragOver = handleDragOver.bind(
         this,
         player
       ))
     );
-
-    console.log(boundEventListeners);
-
-    const shipList = createShipPlacementList();
-    setupContainer.appendChild(shipList);
-    setupContainer.appendChild(setupBoard);
-    document.querySelector('.wrapper').appendChild(setupContainer);
   };
 
-  const clearDragClasses = function (cell) {
-    if (cell.classList.contains('dragged-over-cell')) {
-      cell.classList.remove('dragged-over-cell');
-    }
+  const clearAvailableUnavailableClasses = function (cell) {
     if (cell.classList.contains('available')) {
       cell.classList.remove('available');
     }
@@ -125,83 +137,79 @@ export const gameSetup_UI = (function () {
     }
   };
 
+  const removeFeedbackClassesOnAllCells = function () {
+    document
+      .querySelectorAll('.cell')
+      .forEach(clearAvailableUnavailableClasses);
+  };
+
+  const gatherCells = function (startingPosition, draggingShipLength) {
+    const arr = [];
+    for (let i = 0, l = draggingShipLength; i < l; i++) {
+      arr.push(
+        document.querySelector(`[data-position='${i + startingPosition}']`)
+      );
+    }
+    return arr;
+  };
+
+  const colorAvailable = function (cell) {
+    cell.classList.add('available');
+  };
+
+  const colorUnavailable = function (cell) {
+    cell.classList.add('unavailable');
+  };
+
+  const colorOccupied = function (cell) {
+    cell.classList.add('occupied');
+  };
+
+  const colorCells = function (cells, player) {
+    if (
+      cells.every((cell) => !player.board.cells[cell.dataset.position].occupied)
+    ) {
+      cells.forEach(colorAvailable);
+    }
+    if (
+      cells.some((cell) => player.board.cells[cell.dataset.position].occupied)
+    ) {
+      cells.forEach(colorUnavailable);
+    }
+  };
+
   //* EVENT LISTENERS
   const handleShipNameDrag = function (e) {
-    // console.log(e);
     e.target.classList.add('dragging');
   };
 
-  // const handleShipDrop = function (e) {
-  //   console.log(e);
-  //   e.target.classList.remove('dragging');
-  // };
-
   const handleDragOver = function (player, e) {
+    removeFeedbackClassesOnAllCells();
     if (e.target.classList.contains('cell')) {
-      // console.log(player);
-      // console.log(e);
-      // console.log(this);
-      const allCells = document.querySelectorAll('.cell');
-      allCells.forEach((cell) => {
-        clearDragClasses(cell);
-      });
       const startingCell = e.target;
       const startingPosition = Number(startingCell.dataset.position);
       const draggingShip = document.querySelector('.dragging');
       const draggingShipLength = Number(draggingShip.dataset.length);
 
-      const cells = (() => {
-        const arr = [];
-        for (let i = 0, l = draggingShipLength; i < l; i++) {
-          // console.log(i);
-          const cell = document.querySelector(
-            `[data-position='${i + startingPosition}']`
-          );
-          // cell.classList.add('dragged-over-cell');
-          console.log(player.board.cells[i + startingPosition]);
-          if (player.board.cells[i + startingPosition].occupied) {
-            cell.classList.add('unavailable');
-          } else {
-            cell.classList.add('available');
-          }
-          // console.log(cell);
-          arr.push(
-            document.querySelector(
-              `.cell[data-position='${i + startingPosition}']`
-            )
-          );
-        }
-        return arr;
-      })();
-      // console.log(draggingShip);
+      const cells = gatherCells(startingPosition, draggingShipLength);
+      colorCells(cells, player);
+
       draggingShip.addEventListener('dragend', () => {
-        console.log(cells);
         if (cells.every((cell) => cell.classList.contains('available'))) {
-          console.log('all cells good');
-          console.log(startingCell.dataset.position);
           player.board.placeShip(
-            Number(startingCell.dataset.position),
+            startingPosition,
             draggingShip.dataset.name,
             'horizonal'
           );
-          console.log(player.board.cells);
-          cells.forEach((cell) => {
-            clearDragClasses(cell);
-            cell.classList.add('occupied');
-          });
+          removeFeedbackClassesOnAllCells();
+          cells.forEach(colorOccupied);
           draggingShip.remove();
         }
         if (player.board.ships.length === 5) {
           gameLoop.startGame();
         }
       });
-      // over.classList.add('dragged-over-cell');
-      // over.addEventListener('dragleave', handleDragLeaveCell);
     }
-  };
-
-  const handleDragLeaveCell = function (e) {
-    e.target.classList.remove('dragged-over-cell');
   };
 
   return {
